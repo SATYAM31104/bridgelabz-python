39ce4b32ee86562bbdabc55142e5205e25acec78
shashwat srivastava <113302101+SATYAM31104@users.noreply.github.com>
2025-12-08 15:12:01 +0530

    feat: implement UC5: track dice rolls and positions

--- main.py
+++ main.py
@@ -17,3 +17,93 @@ move_map = {
     2:"Ladder",
     3:"Move"
 }
+# UC2
+noplay = 0
+ladder = 1
+snake = 2
+win = 3
+
+dice = play_map.get(random.randint(0,3),"No play")
+if dice == noplay:
+    print("No play")
+if dice == ladder:
+    print("Move up the ladder")
+elif(dice == snake):
+    print("Move down the snake")
+elif(dice == win):
+    print("Move")
+
+# UC3
+# UC3 & UC4
+pos = 0
+while (pos != 100):
+    dice = random.randint(1, 6)
+    options = random.randint(0, 3)
+    noplay = 0
+    ladder = 1
+    snake = 2
+    win = 3
+    old_pos = pos
+
+    if options == noplay:
+        print("No play")
+    elif options == ladder:
+        print("Move up the ladder")
+        pos += dice
+    elif options == snake:
+        print("Move down the snake")
+        pos -= dice
+    elif options == win:
+        print("Move")
+        pos += dice
+
+    if pos > 100:
+        pos = old_pos
+        print("Stay in the same position")
+
+    if pos < 0:
+        pos = 0
+        print("Restart from 0")
+
+
+# UC5 & UC6
+pos = 0
+roll = 0
+noplay = 0
+ladder = 1
+snake = 2
+win = 3
+
+while (pos != 100):
+    dice = random.randint(1, 6)
+    options = random.randint(0, 3)
+    old_pos = pos
+    print("The dice number is", dice)
+    print("The current position of the player is", pos)
+    print("The option is", options)
+    roll += 1
+
+    if options == noplay:
+        print("No play")
+    elif options == ladder:
+        print("Move up the ladder")
+        pos += dice
+    elif options == snake:
+        print("Move down the snake")
+        pos -= dice
+    elif options == win:
+        print("Move")
+        pos += dice
+
+    if pos > 100:
+        pos = old_pos
+        print("Stay in the same position")
+
+    if pos < 0:
+        pos = 0
+        print("Restart from 0")
+
+    print("The new position of the player is", pos)
+
+print("The total Rolls taken to win", roll)
+
